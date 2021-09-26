492d36b40870d9ba9c489424383305c01bb6452c
Liao CY <liaochungyid@gmail.com>
2021-09-26 14:42:03 +0800

    feat: add a sort dropdown button (but not work)

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,21 +3,38 @@ const router = express.Router()
 
 const Restaurant = require('../../models/restaurantSchema')
 
-  // home page
-  router.get('/', (req, res) => {
-    Restaurant.find()
-      .lean()
-      .sort({ rating: 'desc' })
-      .then(restaurants => {
-        if (restaurants.length === 0) {
-          return res.render('index', { error_msg: '資料庫沒有餐廳！' })
-        }
-        res.render('index', { restaurants })
-      })
-      .catch(err => console.log(err))
-  })
+// home page
+router.get('/', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .sort({ rating: 'desc' })
+    .then(restaurants => {
+      if (restaurants.length === 0) {
+        return res.render('index', { error_msg: '資料庫沒有餐廳！' })
+      }
+      res.render('index', { restaurants })
+    })
+    .catch(err => console.log(err))
+})
 
-// search (home page)
+// sort
+router.get('/search/:field_dir', (req, res) => {
+  const [field, dir] = req.params.field_dir.split('_')
+
+  Restaurant.find()
+    .lean()
+    .sort({ field : dir })
+  .then(restaurants => {
+    console.log(field, 'and', dir, '----')
+    if (!restaurants.length) {
+      return res.render('index', { error_msg: '資料庫沒有餐廳！' })
+    }
+    res.render('index', { restaurants })
+  })
+  .catch(err => console.log(err))
+})
+
+// search
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
   Restaurant.find({
