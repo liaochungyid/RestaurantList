4c136e9459d8ae76802cf3fda9006f17addc62d4
Liao CY <liaochungyid@gmail.com>
2021-10-19 12:48:34 +0800

    feat: add register feature.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/userSchema')
+
 const userController = {
   getLogin: (req, res) => {
     res.render('login')
@@ -9,7 +11,29 @@ const userController = {
     res.render('register')
   },
   postRegister: (req, res) => {
-    res.redirect('/users/login')
+    const { name, email, password, confirmPassword } = req.body
+
+    if (password !== confirmPassword) {
+      console.log('Password and confirm password are not the same.')
+      return res.render('register', {
+        name, email, password, confirmPassword
+      })
+    }
+
+    User.findOne({ email }).then(user => {
+      if (user) {
+        console.log('Email has already been registered.')
+        return res.render('register', {})
+      }
+    })
+
+    User.create({ name, email, password })
+      .then(user => {
+        console.log('User registered successfully.')
+        res.redirect('/users/login')
+      })
+      .catch(err => console.log(err))
+
   }
 }
 
